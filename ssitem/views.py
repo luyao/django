fa53774be87371510a37a5bffe319473498acd7d
Lu Yao <yaolu1103@gmail.com>
2013-08-09 18:32:38 +0800

    comment received!!!

--- ssitem/views.py
+++ ssitem/views.py
@@ -10,48 +10,107 @@ from django.contrib.contenttypes.models import ContentType
 
 from sscomment.models 	import Qcomment
 from ssitem.models 	import Shise
-from sscomment.forms 	import QcommentForm
+from sscomment.forms 	import QcommentForm 
+from ssuser.models import RegistrationProfile
 
-def shiseview(request, sid, form_class=QcommentForm, template_name="shise/jqq_shise.html"):
-	shise = get_object_or_404(Shise, id=sid)
+class urlInfo:
+	url=""
+	name=""
+
+class averageScore:
+	class commentCate:
+		name=""
+		score=5.0
+
+	class userSkinType:
+		skinType=""
+		score=4.0
+		nTotal=1
+
+	total=0.0
+	cate=[]
+	type=[]
 
 
-	qcommentall   = shise.qcomments.all()
+def getCommentCate(scoreList):
+	nameList = ["吸收度", "持久度", "防晕染", "性价比", "色彩度"]
+	list=[]
+	for index in range(len(nameList)):
+		cate = averageScore.commentCate()
+		cate.name = nameList[index]
+		cate.score = scoreList[index]
+		list.append(cate)
+	return list
+
+def getUserSkinType(scoreList):
+	totalList = [52, 46, 29, 13, 11]
+	nameList  = ["混合性", "中性", "油性", "干性", "敏感性"]
+	list=[]
+	for index in range(len(nameList)):
+		cate = averageScore.commentCate()
+		cate.name   = nameList[index]
+		cate.score  = scoreList[index]
+		cate.nTotal = totalList[index]
+		list.append(cate)
+	return list
+
+
+def shiseview(request, sid, form_class=QcommentForm, template_name="shise/item.html"):
+	item = get_object_or_404(Shise, id=sid)
+	creator = object
+	try:
+		creator = item.author
+	except:
+		creator.username="default"
+
+	scoreList = [5.0, 4.8, 3.2, 1.7, 0.5]
+	cntList   = [3.7, 4.8, 5, 2, 1.0]
+	average_score = averageScore()
+	average_score.total = round( sum(scoreList)/len(scoreList), 1)
+	average_score.cate  = getCommentCate(scoreList)
+	average_score.type  = getUserSkinType(cntList)
+
+	item_stick = Shise.objects.all()
+	qcommentall   = item.qcomments.all()
 	qcommentcount = qcommentall.count()
 	qcommentall  = qcommentall[:5]
-	if request.method == 'POST':
-		if request.is_ajax():
-			#处理申请试色请求
-			action = request.POST.get('action', False)
-			if action == 'shise':
-				shise.checkiners.add(request.user)
-				shise.checkin += 1
-				shise.save()
-				message = u"申请 %s 成功！" %shesi.name
-			return HttpResponse(message)
 
+	#for items in the same brand
+	items_in_same_brand = Shise.objects.filter(brand=item.brand)
+	if request.method == 'POST':
+		successurl = '#in'
+		#处理添加评论请求
+		form = form_class(request.POST)
+		if form.is_valid():
+			content = form.cleaned_data.get("content",'')
+			try:
+				qcom = form.GetComment()
+			except:
+				qcom = Qcomment(content=content, lights=10)
+			#qcom = Qcomment(comment=content, has_quote=False, is_public=True, lights=0)
+			qcom.author = request.user
+			qcom.content_object = item 
+			##qcomlist = Qcomment.objects.filter(shise=item, author=request.user)
+			#for oldqcom in qcomlist:
+			#	if qcom.comment == oldqcom.comment:
+			#		qcom = oldqcom
+			#		successurl = '%s#qcomment-area' %(item.get_absolute_url())
+			#		return HttpResponseRedirect(successurl)
+			qcom.save()
+			return HttpResponseRedirect(successurl)
 		else:
-			#处理添加评论请求
-			form = form_class(request.POST)
-			if form.is_valid():
-				content = form.cleaned_data["content"]
-				qcom = Qcomment(comment=content, has_quote=False, is_public=True, lights=0)
-				qcom.author = request.user
-				qcom.content_object = shise
-				qcomlist = Qcomment.objects.filter(shise=shise, author=request.user)
-				for oldqcom in qcomlist:
-					if qcom.comment == oldqcom.comment:
-						qcom = oldqcom
-						successurl = '%s#qcomment-area' %(shise.get_absolute_url())
-						return HttpResponseRedirect(successurl)
-				qcom.save()
-				successurl = '%s#qcomment-area' %(shise.get_absolute_url())
-				return HttpResponseRedirect(successurl)
+			successurl = '%s#postfailed' %(item.get_absolute_url())
+			return HttpResponseRedirect(successurl)
 	else:
 		form = form_class()
 
 	context = RequestContext(request)
 	return render_to_response(template_name, 
-		{'shise':shise,
-		'form':form, 'qcommentlist':qcommentall, 'qcommentcount':qcommentcount},
+		{'item':item,
+		 'item_stick':item_stick,
+		 'items_in_same_brand':items_in_same_brand,
+		 'top_offset':1800,
+		 'average_score':average_score,
+		 'creator': creator,
+		 'form':form, 'qcommentlist':qcommentall, 'qcommentcount':qcommentcount},
 		context_instance=context)
